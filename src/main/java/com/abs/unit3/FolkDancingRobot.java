950cebabf8ea96965034354515af318b823cc8c9
ekaterina.afanasieva <catharine.afanasyeva@gmail.com>
2023-01-30 20:50:48 +0200

    commit1

---
+++ src/main/java/com/abs/unit3/FolkDancingRobot.java
@@ -0,0 +1,13 @@
+package com.abs.unit3;
+
+public class FolkDancingRobot extends DancingRobot{
+    @Override
+    public String getDanceType1() {
+        return "Bla bla";
+    }
+
+    @Override
+    public String getDanceType2() {
+        return "Bla Bla2222";
+    }
+}
---
+++ src/main/java/com/abs/unit3/Main.java
@@ -0,0 +1,19 @@
+package com.abs.unit3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DancingRobot robot = new BalletDancingRobot();
+        DancingRobot robot1 = new FolkDancingRobot();
+
+        printRobotDanceTypes(robot);
+        printRobotDanceTypes(robot1);
+    }
+
+    private static void printRobotDanceTypes(DancingRobot robot) {
+        System.out.println(robot.getDanceType1());
+        System.out.println(robot.getDanceType2());
+        System.out.println(robot.getDefaultType());
+        System.out.println("++++++++++++++++++++\n");
+    }
+}
