3a4f2d77e9637ca6a2c275c33834fec8c3b18dab
ekaterina.afanasieva <catharine.afanasyeva@gmail.com>
2023-01-30 22:29:31 +0200

    commit3

---
+++ src/main/java/com/abs/unit3/JazzDancingRobot.java
@@ -0,0 +1,19 @@
+package com.abs.unit3;
+
+public class JazzDancingRobot extends DancingRobot{
+
+    @Override
+    public String getDefaultType() {
+        return getDanceType2();
+    }
+
+    @Override
+    public String getDanceType1() {
+        return "jazz dance 1";
+    }
+
+    @Override
+    public String getDanceType2() {
+        return "jazz dance 2";
+    }
+}
--- src/main/java/com/abs/unit3/Main.java
+++ src/main/java/com/abs/unit3/Main.java
@@ -3,17 +3,18 @@ package com.abs.unit3;
 public class Main {
     public static void main(String[] args) {
 
-        BalletDancingRobot robot = new BalletDancingRobot();
-
+        BalletDancingRobot robot = new BalletDancingRobot(); // var robot = new BalletDancingRobot();
         printRobot(robot);
+
         printRobot(new FolkDancingRobot());
+        printRobot(new JazzDancingRobot());
     }
 
     private static void printRobot(DancingRobot robot) {
         System.out.println("++++++++++++++++++++");
-        System.out.println("Dance type1" + robot.getDanceType1());
-        System.out.println("Dance type2" + robot.getDanceType2());
-        System.out.println("Default Dance type" + robot.getDefaultType());
+        System.out.println("Dance type1 " + robot.getDanceType1());
+        System.out.println("Dance type2 " + robot.getDanceType2());
+        System.out.println("Default Dance type " + robot.getDefaultType());
         System.out.println("++++++++++++++++++++\n");
     }
 }
